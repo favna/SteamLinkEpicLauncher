f6b44938c9a8260a3100f45cd6ac4063af6b64be
Tyler Davis <tgfdavis98@gmail.com>
2021-04-26 00:12:46 +0100

    make it truly single file and trimmed

--- build.ps1
+++ build.ps1
@@ -5,4 +5,4 @@ Remove-Item -Force -Recurse -Path .\obj -ErrorAction Ignore
 Remove-Item -Force -Recurse -Path .\out -ErrorAction Ignore
 
 # Publish to single exe
-dotnet publish -r win10-x64 -p:PublishSingleFile=true -p:DebugType=None --self-contained true -c Release -o out
+dotnet publish -r win10-x64 -p:PublishTrimmed=true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:DebugType=None --self-contained true -c Release -o out
