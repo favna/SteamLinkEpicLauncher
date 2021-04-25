c5d363c4a583fd86669a56a7892f03c3d44337bf
Tyler Davis <tgfdavis98@gmail.com>
2021-04-25 21:53:57 +0100

    change Program.cs to use top level statements, remove redundant namespace qualifier, and use Console.Error

--- src/Program.cs
+++ src/Program.cs
@@ -1,28 +1,20 @@
 ﻿using System;
 using System.Diagnostics;
 
-namespace SteamLauncher
+
+if (args.Length == 0)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            if (args.Length != 1)
-            {
-                System.Console.WriteLine("ERROR: Needs launch URL");
-                return;
-            }
-
-            var epicUrl = args[0];
-
-            var ps = new ProcessStartInfo(epicUrl)
-            {
-                UseShellExecute = true,
-                Verb = "open"
-            };
-
-            System.Console.WriteLine($"Starting url: {epicUrl}");
-            Process.Start(ps);
-        }
-    }
+    Console.Error.WriteLine("ERROR: Needs launch URL");
+    return;
 }
+
+var epicUrl = args[0];
+
+var ps = new ProcessStartInfo(epicUrl)
+{
+    UseShellExecute = true,
+    Verb = "open"
+};
+
+Console.WriteLine($"Starting url: {epicUrl}");
+Process.Start(ps);
